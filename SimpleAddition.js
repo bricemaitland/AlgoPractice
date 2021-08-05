c6e397c1ede853a2c43986fc1350a9cf3975564c
Brice Maitland <brice.maitland@yahoo.com>
2021-08-05 13:40:36 -0700

    Simple Addition Algo

---
+++ SimpleAddition.js
@@ -0,0 +1,14 @@
+// Complete the solveMeFirst function below.
+//int a = first value
+//int b = second value
+//Returns an integer: the sum of a and b
+//sample a = 2, b = 3
+//sample output = 5
+
+
+a = 2;
+b = 3;
+function solveMeFirst(a, b) {
+return a+b;
+}
+console.log(solveMeFirst(a,b));
